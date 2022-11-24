701b95bb79d3acdbbc304ae2ab6d725958e5941f
Iddahodhiambo <iddahawuor@yahoo.com>
2022-11-24 23:18:14 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function spy(name) {
+    return `hello, I am ${name}.`
+}
+function receivesAFunction(callback) {
+    callback()
+}
+
+const returnsANamedFunction = () => {
+    const fn  = () => 5 + 8;
+    return fn;
+}
+const fn = returnsANamedFunction();
+
+
+function returnsAnAnonymousFunction(){
+    return () =>{
+        console.log("Welcome to GeeksforGeeks!");
+    };
+}
